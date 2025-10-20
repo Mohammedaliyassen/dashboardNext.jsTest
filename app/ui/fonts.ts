b9948fc1265ce7928a17686806d3bd8cdfb2face
Mohammedaliyassen <mido200117@gmail.com>
2025-10-21 02:41:56 +0300

    fonts error

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,7 +3,8 @@ import { Inter, Cairo, Lusitana } from 'next/font/google';
 export const inter = Inter({ subsets: ['latin'] });
 export const cairo =
     Cairo({
-        weight: ['400', '700']
+        weight: '400',
+        subsets: ['arabic', 'latin']
     });
 export const lusitana =
     Lusitana({
