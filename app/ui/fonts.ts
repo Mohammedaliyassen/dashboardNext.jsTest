296d299cb82d923d03dcdf52cf12d26bbfbf74be
Mohammedaliyassen <mido200117@gmail.com>
2025-10-20 23:21:14 +0300

    last files

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Cairo, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const cairo =
+    Cairo({
+        weight: ['400', '700']
+    });
+export const lusitana =
+    Lusitana({
+        subsets: ['latin'],
+        weight: ['400', '700']
+    });
